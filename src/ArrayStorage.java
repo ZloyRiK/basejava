2e039d6849f0d2d9840d9144e825a4d446e5b6fa
ZloRiK <r.zrk@yandex.ru>
2023-05-18 11:31:41 +0500

    Реализованы методы save, get, delete, clear, getAll, size в соответствии с домашним заданием. Все тесты успешны

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,64 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int endPoint = 0; // first empty index in array
+    //Инициирован нулем потому что первой будет операция сохранения и там сразу можно считать его
 
     void clear() {
+        for (int i = 0; i < endPoint; i++) {
+//            if (storage[i] != null) storage[i] = null; по сути проверка даже не нужна
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[endPoint] = r;
+        endPoint++;
     }
 
     Resume get(String uuid) {
-        return null;
+        int temp = findIndex(uuid);
+        if (temp == -1) {
+            Resume r = new Resume();
+            r.uuid = "Not this time";
+            return r;
+        } else return storage[temp];
     }
 
     void delete(String uuid) {
+        int delIndex = findIndex(uuid);
+        storage[delIndex] = null;
+
+        while (delIndex != endPoint) {
+            storage[delIndex] = storage[delIndex + 1];
+            delIndex++;
+        }
+        endPoint--;
+    }
+
+    private int findIndex(String uuid) {
+        int index = 0;
+        for (int i = 0; i < endPoint; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                index = i;
+                break;
+            } else index = -1;
+        }
+        return index;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[endPoint];
+        for (int i = 0; i < endPoint; i++) {
+            result[i] = storage[i];
+        }
+        return result;
     }
 
     int size() {
-        return 0;
+        return endPoint;
     }
 }
